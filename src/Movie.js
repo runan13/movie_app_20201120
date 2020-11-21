06e124b72613344b7ed66d109d35d34e3fb394cf
runan13 <65391973+runan13@users.noreply.github.com>
2020-11-21 21:46:51 +0900

    CSS Styling

--- src/Movie.js
+++ src/Movie.js
@@ -9,14 +9,16 @@ function Movie({ year, title, summary, poster, genres }) {
       <div className="movie__data">
         <h3 className="movie__title">{title}</h3>
         <h5 className="movie__year">{year}</h5>
-        <ul className="genres">
-          {genres.map((genre, index) => (
-            <li key={index} className="genres__genre">
-              {genre}
-            </li>
-          ))}
+        <ul className="movie__genres">
+          {genres
+            .map((genre, index) => (
+              <li key={index} className="genres__genre">
+                {genre}
+              </li>
+            ))
+            .slice(0, 3)}
         </ul>
-        <p className="movie__summary">{summary}</p>
+        <p className="movie__summary">{summary.slice(0, 140)}...</p>
       </div>
     </div>
   );
